7164f58a42aa2d16b4cdb6dc14f856b9fe42bc66
benjsnellings <bsnellings@cyberpointllc.com>
2017-05-24 14:29:55 -0400

    Changed colors to purple and added workspace 2 setup

--- i3/setup_workspace.sh
+++ i3/setup_workspace.sh
@@ -5,6 +5,10 @@ i3-sensible-terminal
 i3-sensible-terminal 
 i3-sensible-terminal
 
+i3-msg "workspace 2; append_layout ~/.i3/workspace-2.json"
+i3-msg "exec --no-startup-id firefox"
+i3-msg "exec --no-startup-id ~/Documents/IntelliJ/idea-IC-163.11103.6/bin/idea.sh"
+
 i3-msg "workspace 3; append_layout ~/.i3/workspace-3.json"
 i3-msg "exec --no-startup-id slack"
 
