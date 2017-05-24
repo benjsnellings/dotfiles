373a6e341089cd01a3638f05fd05df26291c6764
benjsnellings <bsnellings@cyberpointllc.com>
2017-05-24 11:56:14 -0400

    added some workspace preset configurations

---
+++ i3/setup_workspace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+i3-msg "workspace 1; append_layout ~/.i3/workspace-1.json"
+i3-sensible-terminal 
+i3-sensible-terminal 
+i3-sensible-terminal
+
+i3-msg "workspace 3; append_layout ~/.i3/workspace-3.json"
+i3-msg "exec --no-startup-id slack"
+
+i3-msg "workspace 4"
+i3-msg "exec --no-startup-id spotify"
