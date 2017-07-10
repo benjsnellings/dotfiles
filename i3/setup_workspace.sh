adb583bfadcc93a598e621bc0def05b81a9433d3
benjsnellings <bsnellings@cyberpointllc.com>
2017-07-10 12:54:12 -0400

    moved terminals to different monitor

--- i3/setup_workspace.sh
+++ i3/setup_workspace.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-i3-msg "workspace 1; append_layout ~/.i3/workspace-1.json"
+i3-msg "workspace 2; append_layout ~/.i3/workspace-1.json"
 i3-sensible-terminal 
 i3-sensible-terminal 
 i3-sensible-terminal
 
-i3-msg "workspace 2; append_layout ~/.i3/workspace-2.json"
+i3-msg "workspace 1; append_layout ~/.i3/workspace-2.json"
 i3-msg "exec --no-startup-id firefox"
 i3-msg "exec --no-startup-id ~/Documents/IntelliJ/idea-IC-163.11103.6/bin/idea.sh"
 
