cc24e43f29915790a67c49e3ff04c22b3df93830
Benjamin Snellings <snellin@amazon.com>
2018-10-18 11:59:50 -0700

    Added vimrc and ideavimrc to dotfiles

--- manageDotfiles.sh
+++ manageDotfiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc i3 i3status.conf dir_colors polybar rofi"    # list of files/folders to symlink in homedir
+files="zshrc i3 i3status.conf dir_colors polybar rofi vimrc ideavimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
---
+++ vimrc
@@ -0,0 +1,21 @@
+
+
+syntax enable
+set tabstop=4
+set softtabstop=4
+set expandtab
+set number
+set showcmd
+set cursorline
+filetype indent on
+
+inoremap jk <esc>
+
+xnoremap <leader>p "_dP
+
+noremap <C-p> viw"_dP
+
+
+
+
+
