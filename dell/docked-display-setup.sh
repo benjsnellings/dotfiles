cf0cd4567e96d1ade24b64df72c7da9231b1a05d
Benjamin Snellings <snellin@amazon.com>
2020-10-09 22:22:36 -0700

    Changes to consolidate desktop setup scripts into one

---
+++ dell/docked-display-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 $HOME/.i3/screen_setup.py
+python3 $HOME/.i3/setup_workspace.py
+nitrogen --restore
+~/.config/polybar/launch.sh
+~/dotfiles/dell/dell_setup.sh
+
--- vimrc
+++ vimrc
@@ -24,6 +24,7 @@ xnoremap <leader>p "_dP
 
 noremap <C-p> viw"_dP
 
+set clipboard=unnamedplus
 
 
 
