76dafccb7d25146db2bc3034dabd1c4dbff90101
Ben Snellings <snellin@amazon.com>
2024-05-13 10:37:44 -0700

    Update dot_zsh/03-amazon.zsh Update private_dot_config/starship.toml

--- dot_zsh/03-amazon.zsh
+++ dot_zsh/03-amazon.zsh
@@ -76,7 +76,7 @@ function ada_calculator() {
    export AWS_SESSION_TOKEN=$(echo ${ada_output} | jq -r .SessionToken)
 }
 
-function ada_snellin_demo() {
+function ada_snellin_ac_demo() {
    ada_output=$(ada credentials print --account 471112920131 --role IibsAdminAccess-DO-NOT-DELETE --provider conduit) 
    export AWS_ACCESS_KEY_ID=$(echo ${ada_output} | jq -r .AccessKeyId) 
    export AWS_SECRET_ACCESS_KEY=$(echo ${ada_output} | jq -r .SecretAccessKey) 
