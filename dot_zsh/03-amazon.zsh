7e9071c1acdc5811f4760ec4ca51630dc002e6ee
Ben Snellings <snellin@amazon.com>
2025-03-26 15:48:54 -0700

    Update .zsh/03-amazon.zsh

--- dot_zsh/03-amazon.zsh
+++ dot_zsh/03-amazon.zsh
@@ -144,7 +144,7 @@ function ada_conduit() {
   	return 1
    fi
 
-   ada_output=$(ada credentials print --account $1 --role Admin --provider conduit) 
+   ada_output=$(ada credentials print --account $1 --role IibsAdminAccess-DO-NOT-DELETE --provider conduit) 
    export AWS_ACCESS_KEY_ID=$(echo ${ada_output} | jq -r .AccessKeyId) 
    export AWS_SECRET_ACCESS_KEY=$(echo ${ada_output} | jq -r .SecretAccessKey) 
    export AWS_SESSION_TOKEN=$(echo ${ada_output} | jq -r .SessionToken)
